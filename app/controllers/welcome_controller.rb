aa95ce0830e37a625a1674103e415677a4a4c70d
Mukul B. Patel <Mukul215@gmail.com>
2014-06-21 17:34:13 -0400

    Added text field and updated routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
   	@planets = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
+  	@user = User.new
   end
-  
 end
