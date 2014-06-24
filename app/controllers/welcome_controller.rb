5138ae02733a447bd8f5221a7fb18616af1e24d8
Mukul B. Patel <Mukul215@gmail.com>
2014-06-24 19:56:58 -0400

    Added ability to add user to DB

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,20 @@ class WelcomeController < ApplicationController
   end
 
   def create_account
-    #raise params.inspect
-    render :text => 'thanks for signing up!'
+    # raise params.inspect (*only used for development and not deployment*)
+    # raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+    	render :text => 'thanks for signing up!'
+    else
+    	render 'new'
+    end
+
+
   end
 
+  private
+  	def user_params
+			params.require(:user).permit(:name, :from_planet, :wants_planet)
+		end
 end
