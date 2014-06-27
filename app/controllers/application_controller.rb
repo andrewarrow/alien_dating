e0cfd1a2060c1abd7c0b373680ce45028d53479a
Andrew Arrow <oneone+github@gmail.com>
2014-06-27 11:47:20 -0700

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-    def current_user
-    @current_user ||= session[:current_user_id] &&
-      User.find_by(id: session[:current_user_id])
-    end
+  helper_method :current_user
+
+  def current_user
+    return nil if not session[:user_id]
+    User.find_by_id(session[:user_id])
+  end
+
+  def must_have_user
+    redirect_to root_path if not current_user
+  end
   
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,29 +6,24 @@ class WelcomeController < ApplicationController
   end
 
   def create_account
-    # raise params.inspect (*only used for development and not deployment*)
-    # raise params.inspect
     @user = User.new(user_params)
     if @user.save
-      session[:user] = @user.id
-      # we use this to show dynamic users
-    	redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-    	render 'new'
+      render 'new'
     end
   end
 
   def logout
-    # *only used for development and not deployment*
-    # raise session.inspect
-    session[:user] = nil
+    session[:user_id] = nil
     redirect_to root_path
   end
 
   private
 
-  	def user_params
-			params.require(:user).permit(:name, :from_planet, :wants_planet)
-		end
+  def user_params
+    params.require(:user).permit(:name, :from_planet, :wants_planet)
+  end
 
 end
