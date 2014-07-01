d20db370f203dba700bebb02911b5fe0f340e292
Andrew Arrow <oneone+github@gmail.com>
2014-07-01 11:27:25 -0700

    clean up login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,13 +20,20 @@ class WelcomeController < ApplicationController
     redirect_to root_path
   end
 
+  def login
+    @users = User.all.map { |u| u.name }
+  end
+
   def login_user
-    if @user = User.authenticate(params[:id])
+    user = User.find_by_name(params[:user_id])
+    if user
       session[:user_id] = user.id
-      redirect_to user_path(@user)
-    else
-      render 'new'
+      redirect_to user_path(user)
+      return
     end
+
+    flash[:error] = 'Sorry that user not in the system.'
+    redirect_to root_path
   end
 
   private
